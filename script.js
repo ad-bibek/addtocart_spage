7f60de580bb6c2a059b40b477c524cb479ebf246
ad-bibek <bibekadhikari988@gmail.com>
2024-05-27 11:41:21 +0545

    added images and slider function

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const searchBar = document.getElementById("search-bar");
   let cart = JSON.parse(localStorage.getItem('cart')) || [];
   let products = [];
+  setupSlider();
 
   // Update cart count on page load
   cartCount.textContent = cart.length;
@@ -111,4 +112,29 @@ document.addEventListener("DOMContentLoaded", () => {
     );
     displayProducts(filteredProducts);
   });
+  function setupSlider() {
+    const imgs = document.querySelectorAll('.header-slider ul img');
+    const prev_btn = document.querySelector('.control_prev');
+    const next_btn = document.querySelector('.control_next');
+
+    let n = 0;
+
+    function changeSlide() {
+        imgs.forEach((img, i) => {
+            img.style.display = i === n ? 'block' : 'none';
+        });
+    }
+
+    changeSlide();
+
+    prev_btn.addEventListener('click', () => {
+        n = (n > 0) ? n - 1 : imgs.length - 1;
+        changeSlide();
+    });
+
+    next_btn.addEventListener('click', () => {
+        n = (n < imgs.length - 1) ? n + 1 : 0;
+        changeSlide();
+    });
+}
 });
